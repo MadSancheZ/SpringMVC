be3a6ee1aa9d86ab1247e3af7ee5c7680f0ceb8e
MadSanchez <sanekallas@gmail.com>
2019-03-05 14:10:51 +0300

    private users

--- src/main/java/org/madsanchez/MainController.java
+++ src/main/java/org/madsanchez/MainController.java
@@ -14,7 +14,7 @@ import java.util.List;
 
 @Controller
 public class MainController {
-    static List<User> users = new ArrayList<>();
+    private List<User> users = new ArrayList<>();
 
 
     @GetMapping("/view/{name}")
